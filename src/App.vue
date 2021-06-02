2ca7af8b4d01a4ad03f61be70a47ed32e77dd42a
minhpt6 <minhtp@lattehub.com>
2021-06-02 17:50:25 +0700

    add new

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Login/>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
