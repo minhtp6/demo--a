df23a44cc2ead67a21d2318e4ca7cdf5b2c9bc1d
Minh Phạm <phamminh591@gmail.com>
2021-06-03 19:48:50 +0700

    add todolist

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,52 @@
 <template>
-<div>
-    <h1> aaaa</h1>
-</div>
+  <div id="app">
+    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import sweetalert from 'sweetalert';
+import TodoList from './TodoList';
+import CreateTodo from './CreateTodo';
+
 export default {
-  name: 'Home',
-
-}
+  name: 'app',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-body{
-    background: #edf1f5;
-    margin-top:20px;
-}
-</style>
