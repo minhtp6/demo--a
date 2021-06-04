886c86c134cf7153bef07c4a0f79aa7b990ff597
minhpt6 <minhtp@lattehub.com>
2021-06-04 09:14:10 +0700

    add list todo js not run

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,20 +2,65 @@
   <div>
     <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo ></todo>
-    
+    <!-- <todo v-for= 'todo in todos' v-bind:key="todo" v-on:deleteTodo="deleteTodo" v-on:completeTodo="completeTodo"> </todo> -->
+    <div class='ui centered card' v-for= 'todo in todos' v-bind:key="todo" v-on:deleteTodo="deleteTodo" v-on:completeTodo="completeTodo">
+      <div class="content" v-show="!isEditing">
+      <div class='header'>
+          {{ todo.title }}
+      </div>
+      <div class='meta'>
+          {{ todo.project }}
+      </div>
+      <div class='extra content'>
+          <span class='right floated edit icon' v-on:click="showForm">
+          <i class='edit icon'></i>
+        </span>
+        <span class='right floated trash icon' v-on:click="deleteTodo(todo)">
+          <i class='trash icon'></i>
+        </span>
+      </div>
+    </div>
+    <div class="content" v-show="isEditing">
+      <div class='ui form'>
+        <div class='field'>
+          <label>Title</label>
+          <input type='text' v-model="todo.title" >
+        </div>
+        <div class='field'>
+          <label>Project</label>
+          <input type='text' v-model="todo.project" >
+        </div>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click="hideForm">
+            Close X
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" disabled>
+        Completed
+    </div>
+    <div class='ui bottom attached red basic button' v-on:click="completeTodo(todo)" v-show="!isEditing && !todo.done">
+        Pending
+    </div>
+  </div>
   </div>
 </template>
 
 <script type = "text/javascript" >
 import sweetalert from 'sweetalert';
-import Todo from './Todo.vue';
+// import Todo from './Todo.vue';
 
 export default {
   props: ['todos'],
   components: {
-    Todo
+    //  Todo
   },
+  data() {
+      return {
+        isEditing: false,
+      };
+    },
   methods: {
     deleteTodo(todo) {
       sweetalert({
@@ -30,12 +75,14 @@ export default {
       () => {
         const todoIndex = this.todos.indexOf(todo);
         this.todos.splice(todoIndex, 1);
+        this.$emit('delete-todo', todo);
         sweetalert('Deleted!', 'Your To-Do has been deleted.', 'success');
       });
     },
     completeTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos[todoIndex].done = true;
+      this.$emit('complete-todo', todo);
       sweetalert('Success!', 'To-Do completed!', 'success');
     },
   },
