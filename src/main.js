b6b7d824f4b7145b48404f7182bb47fc890fa75f
minhpt6 <minhtp@lattehub.com>
2021-06-10 16:42:22 +0700

    fix UI

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import Home from './components/Home';
 import Login from './components/Login';
 import VModal from 'vue-js-modal'
 import VueLoading from 'vuejs-loading-plugin'
-import VueMq from 'vue-mq'
 Vue.use(VueRouter)
 Vue.use(Vuex)
 Vue.use(VModal, {
@@ -14,13 +13,6 @@ Vue.use(VModal, {
     adaptive: true,
     maxHeight: 100,
     maxWidth: 315
-  }
-})
-Vue.use(VueMq,{
-  breakpoints: {
-    sm: 450,
-    md: 1250,
-    lg: Infinity,
   }
 })
 Vue.use(VueLoading)
